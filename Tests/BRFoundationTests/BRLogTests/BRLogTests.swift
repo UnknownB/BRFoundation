0bbafc745848f4fda81fe58293f447c836dddbc5
BR <snkp002@gmail.com>
2026-04-29 23:09:33 +0800

    fix: 修復 BRLogTests 未同步語法變更

--- Tests/BRFoundationTests/BRLogTests/BRLogTests.swift
+++ Tests/BRFoundationTests/BRLogTests/BRLogTests.swift
@@ -14,39 +14,19 @@ struct BRLogTests {
     
     @Test("format")
     func testLogFormat() {
-        let log = BRLog.format("admin: BR", "password: ******", tag: "Test")
-        #expect(log == "[Test] BRLogTests・17 -- admin: BR, password: ******")
-    }
-    
-    
-    @Test("printLog")
-    func testPrintLog() {
-        BRLog.printTest.debug("debug")
-        BRLog.printTest.info("info")
-        BRLog.printTest.notice("notice")
-        BRLog.printTest.error("error")
-        BRLog.printTest.fault("fault")
-        let appLog = BRLog.fetchPrintLog()
-        
-        var logs = ""
-        logs += "🛠️ [Test] BRLogTests・24 -- debug\n"
-        logs += "⚙️ [Test] BRLogTests・25 -- info\n"
-        logs += "☑️ [Test] BRLogTests・26 -- notice\n"
-        logs += "❌ [Test] BRLogTests・27 -- error\n"
-        logs += "⚠️ [Test] BRLogTests・28 -- fault\n"
-
-        #expect(appLog == logs)
+        let log = BRLog.format(.init(category: .test, level: .debug, items: ["item"], file: #file, function: #function, line: #line))
+        #expect(log == "🛠️ [Test] BRLogTests・17 -- item")
     }
     
     
     @available(iOS 15.0, *)
     @Test("OSLog")
     func testOSLog() throws {
-        BRLog.test.debug("debug")
-        BRLog.test.info("info")
-        BRLog.test.notice("notice")
-        BRLog.test.error("error")
-        BRLog.test.fault("fault")
+        #BRLog(.test, .debug, "debug")
+        #BRLog(.test, .info, "info")
+        #BRLog(.test, .notice, "notice")
+        #BRLog(.test, .error, "error")
+        #BRLog(.test, .fault, "fault")
         
         let logs = try BRLog.fetchOSLogStore()
         #expect(logs.count >= 5)
