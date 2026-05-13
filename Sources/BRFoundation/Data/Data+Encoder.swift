1758c380ac2748d2236defcc9f9a71f86e1a848b
BR <snkp002@gmail.com>
2026-05-13 22:07:07 +0800

    feat: 更新 Data+Encoder 添加 AES-GCM 加解密功能

--- Sources/BRFoundation/Data/Data+Encoder.swift
+++ Sources/BRFoundation/Data/Data+Encoder.swift
@@ -12,7 +12,7 @@ import CryptoKit
 public extension BRWrapper where Base == Data {
     
     
-    /// 將 Data 轉換為 MD5 十六進制字串
+    /// 將 Data 轉換為 MD5 十六進制字串，僅用於確保資訊一致性 (非安全加密用)
     var md5: String {
         let digest = Insecure.MD5.hash(data: base)
         return digest.map { String(format: "%02x", $0) }.joined()
@@ -23,6 +23,23 @@ public extension BRWrapper where Base == Data {
     var sha256: String {
         let digest = SHA256.hash(data: base)
         return digest.map { String(format: "%02x", $0) }.joined()
+    }
+    
+    
+    /// AES-GCM 加密
+    func aesGCMEncrypt(key: SymmetricKey) throws -> Data {
+        let sealedBox = try AES.GCM.seal(base, using: key)
+        guard let combined = sealedBox.combined else {
+            throw BRFileError.encryptionFailed
+        }
+        return combined
+    }
+    
+    
+    /// AES-GCM 解密
+    func aesGCMDecrypt(key: SymmetricKey) throws -> Data {
+        let sealedBox = try AES.GCM.SealedBox(combined: base)
+        return try AES.GCM.open(sealedBox, using: key)
     }
     
 
