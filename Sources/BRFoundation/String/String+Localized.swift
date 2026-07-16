a46711ed680820518e0be1389123c2f6fd7a0b3a
BR <snkp002@gmail.com>
2026-07-17 00:39:22 +0800

    refactor: 清除編譯警告

--- Sources/BRFoundation/String/String+Localized.swift
+++ Sources/BRFoundation/String/String+Localized.swift
@@ -11,7 +11,7 @@ public extension BRWrapper where Base == String {
 
     
     /// 轉換為本地化字串
-    public var localized: String {
+    var localized: String {
        return NSLocalizedString(base, comment: "")
     }
 
